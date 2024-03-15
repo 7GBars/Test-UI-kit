48ebded57b6f54d934287909433682f5cf7479dd
Barsegyan <barsegyan@topsystems.ru>
2024-03-15 19:07:52 +0300

    -поправить относительный url для logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 
 export const Logo = () => {
-  const logo = new URL('./Add_small.svg', import.meta.url);
+  const logo = new URL('./assets/images/Add_small.svg', import.meta.url);
   return (
     <img src={logo.toString()} alt={'test logo'}/>
   );
