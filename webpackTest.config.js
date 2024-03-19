67629679b59f94a30bcffe5d5a2094543eca5b6e
Barsegyan <barsegyan@topsystems.ru>
2024-03-19 21:20:08 +0300

    -указать файл конфигурации для скрипта -установить лоадеры для css -поднять версию

--- webpackTest.config.js
+++ webpackTest.config.js
@@ -25,6 +25,10 @@ module.exports = {
                 test: /\.(ts|tsx)?$/,
                 use: ['ts-loader'],
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css/,
+                use: ['style-loader', 'css-loader'],
             }
         ]
     },
