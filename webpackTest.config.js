d8fc444d564388e026e256b012b566da76ae9ea5
Barsegyan <barsegyan@topsystems.ru>
2024-03-21 17:04:22 +0300

    -обновить ts-конфиг и webpack

--- webpackTest.config.js
+++ webpackTest.config.js
@@ -63,7 +63,7 @@ module.exports = {
     resolve: {
         alias: {
             "geo_bars_ui_kit_lib": path.resolve(__dirname, 'dist'), // Алиас для вашей библиотеки
-            "@components": path.resolve('src/components') // Алиас для ссылки на ваши компоненты
+            "@geo_components": path.resolve('src/components') // Алиас для ссылки на ваши компоненты
         },
         extensions: ['.ts', '.tsx', '.js'], // Добавьте расширения файлов, если нужно
     },
